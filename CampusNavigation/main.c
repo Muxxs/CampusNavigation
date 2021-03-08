e1fb10f680b0a43a012d18a5f10b2a8cea0f004b
Muxxs <747306970@qq.com>
2021-03-08 22:24:08 +0800

    Initial Commit

---
+++ CampusNavigation/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CampusNavigation
+//
+//  Created by Muxxs🇨🇳 on 2021/3/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
